a4a24ee99451e4fdb062fcd6fc4812bf431eb123
Willie Chalmers III <willieechalmers@gmail.com>
2018-03-11 00:25:51 -0600

    Migrate App to EatShuffleApp

---
+++ src/EatShuffleApp.vue
@@ -0,0 +1,28 @@
+<template>
+  <md-app>
+    <md-app-toolbar class="md-primary">
+      <div class="md-toolbar-row">
+        <span class="md-title">EatShuffle</span>
+      </div>
+    </md-app-toolbar>
+    <md-app-content>
+      <router-view></router-view>
+    </md-app-content>
+  </md-app>
+</template>
+
+<style>
+.md-app-content {
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
+  text-align: center;
+  min-height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.md-app-content::before {
+  background: url('assets/background.jpg');
+  background-size: auto 100%;
+}
+</style>
