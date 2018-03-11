320327e697056c3e8c21a18efbefa8d6980d3dd4
Willie Chalmers III <willieechalmers@gmail.com>
2018-03-11 01:30:18 -0600

    Update router  - Use EatChoiceCard instead of Home  - Use default export declaration to allow modification of router

--- src/router.js
+++ src/router.js
@@ -1,16 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import EatChoiceCard from './components/EatChoiceCard.vue';
 import About from './views/About.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: EatChoiceCard,
     },
     {
       path: '/about',
@@ -19,3 +20,7 @@ export default new Router({
     },
   ],
 });
+
+export default {
+  router,
+};
