bae568a86dca5da1405697b527e71a27e7a8b539
Willie Chalmers III <willieechalmers@gmail.com>
2018-03-11 23:55:38 -0500

    Update router to include results page

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import EatChoiceCard from './components/EatChoiceCard.vue';
 import About from './views/About.vue';
+import ResultsPage from './views/ResultsPage.vue';
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: EatChoiceCard,
+    },
+    {
+      path: '/results',
+      name: 'results',
+      component: ResultsPage,
     },
     {
       path: '/about',
