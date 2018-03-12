1f6f09edfba1392feb04c64b57c268744f5c77d7
Willie Chalmers III <willieechalmers@gmail.com>
2018-03-11 23:55:56 -0500

    Add about view to router

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,59 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <md-card>
+    <section id="project">
+      <md-subheader class="md-primary">About EatShuffle</md-subheader>
+      <div class="description-box">
+        <p>EatShuffle was built over a weekend for the families that could never
+          decide what to eat out.
+        </p>
+        <md-button class="md-primary learn-more" href="https://sites.google.com/view/willie-projects/shuffleeats">
+          Learn more
+        </md-button>
+      </div>
+    </section>
+    <section id="author">
+      <md-subheader class="md-primary">I am Willie Chalmers III</md-subheader>
+      <div class="description-box">
+        <p>I am a freelance programmer who makes apps and websites for education
+          and enjoyment.
+        </p>
+      </div>
+    </section>
+    <section id="social">
+      <md-subheader class="md-primary">Social links</md-subheader>
+      <div class="social-list">
+        <span>Projects site: <a href="https://sites.google.com/view/willie-projects/">Willie's Project Showcase</a></span>
+        <span>Twitter: <a href="https://twitter.com/@WillieCubed">@WillieCubed</a></span>
+      </div>
+    </section>
+  </md-card>
 </template>
+<script>
+export default {
+  name: 'about-page',
+};
+</script>
+
+<style scoped>
+* {
+  text-align: start;
+}
+
+span {
+  display: inline-block;
+}
+
+.md-card {
+  padding: 16px;
+}
+
+.description-box, .social-list {
+  text-align: left;
+  max-width: 368px;
+  padding: 0px 16px;
+  margin-bottom: 16px;
+}
+
+.learn-more {
+}
+</style>
