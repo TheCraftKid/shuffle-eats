370b748b4b65835a183abad10301274eeba21988
Willie Chalmers III <willieechalmers@gmail.com>
2018-03-13 00:58:15 -0500

    Setup SearchBox for future use

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <md-card class="search">
+    <input v-model="searchQuery">
+    <label for="">{{ label }}</label>
+  </md-card>
+</template>
+
+<script>
+export default {
+  name: 'search-box',
+  props: [
+    'label',
+  ],
+  data() {
+    return {
+      searchQuery: '',
+    };
+  },
+  methods: {
+    search() {
+    },
+  },
+};
+</script>
+
+<style scoped>
+input:focus {
+  outline: 0;
+}
+
+.md-card {
+  padding: 2px 8px;
+  margin: 8px;
+}
+</style>
