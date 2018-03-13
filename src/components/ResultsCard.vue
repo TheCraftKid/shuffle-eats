4c90bef21e1b4eb26acab54d0869a96565e4ff09
Willie Chalmers III <willieechalmers@gmail.com>
2018-03-13 01:03:36 -0500

    Add ResultsCard for displaying main generated restaraunt result

---
+++ src/components/ResultsCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <md-card class="results-card">
+    <md-card-header>
+      <div class="md-subhead">You're eating at...</div>
+      <div class="md-title">{{ result.name }}</div>
+    </md-card-header>
+    <md-card-actions>
+      <md-button>Another one</md-button>
+      <md-button class="md-primary">Learn more</md-button>
+    </md-card-actions>
+  </md-card>
+</template>
+
+<script>
+export default {
+  name: 'results-card',
+  props: [
+    'result',
+  ],
+};
+</script>
+
+<style>
+.md-card {
+  padding: 8px;
+}
+</style>
