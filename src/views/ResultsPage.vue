05214fbb10cf7b3e6c8d8ca368e0755848a59152
Willie Chalmers III <willieechalmers@gmail.com>
2018-03-13 00:57:57 -0500

    Create ResultsPage to house request results and recent requests

---
+++ src/views/ResultsPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <section id="results">
+    <results-card :result="firstResult"></results-card>
+    <recent-results></recent-results>
+  </section>
+</template>
+
+<script>
+import ResultsCard from '@/components/ResultsCard.vue';
+import RecentResults from '@/components/RecentResults.vue';
+
+export default {
+  name: 'results-page',
+  components: {
+    'results-card': ResultsCard,
+    'recent-results': RecentResults,
+  },
+  computed: {
+    result() {
+      return {
+        name: 'hello',
+      };
+    },
+  },
+
+};
+</script>
+
+<style scoped>
+.recent-results {
+  margin-top: 16px;
+}
+</style>
